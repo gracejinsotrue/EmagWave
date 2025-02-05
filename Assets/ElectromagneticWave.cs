61219b6ce51d670cd8ccced70f235767aed13813
gracejinsotrue <gjin3234@gmail.com>
2025-02-05 16:04:30 -0500

    add working amplitude slider scripting

--- Assets/ElectromagneticWave.cs
+++ Assets/ElectromagneticWave.cs
@@ -1,8 +1,10 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ElectromagneticWave : MonoBehaviour
 {
-    public int points = 100;      // Number of points in the wave
+    //default numerical assingments but they are bound to change based on slider
+    public int points = 100;      
     public float wavelength = 5f; // Wavelength of the wave
     public float amplitude = 2f;  // Amplitude of the wave
     public float speed = 2f;      // Propagation speed
@@ -11,8 +13,30 @@ public class ElectromagneticWave : MonoBehaviour
     private LineRenderer magneticField;
     private float time = 0f;
 
+    public Slider amplitudeSlider; // assignment for Amplitude Slider
+
     void Start()
     {
+        //Here is the place where I am putting slider code
+        // Automatically find the slider in the scene if not assigned
+        if (amplitudeSlider == null)
+        {
+            amplitudeSlider = FindObjectOfType<Slider>();
+        }
+
+        if (amplitudeSlider != null)
+        {
+            amplitudeSlider.onValueChanged.AddListener(value => UpdateAmplitude(value));
+        }
+        else
+        {
+            Debug.LogError("Amplitude slider not found in the scene. Please assign it in the Inspector.");
+        }
+
+
+
+
+
         // Create and configure LineRenderers for E and B fields
         electricField = CreateLineRenderer(Color.red);   // Red for Electric Field
         magneticField = CreateLineRenderer(Color.blue);  // Blue for Magnetic Field
@@ -21,6 +45,8 @@ public class ElectromagneticWave : MonoBehaviour
     void Update()
     {
         time += Time.deltaTime * speed;
+
+        // Draw the waves with the current amplitude
         DrawWave(electricField, Vector3.right, amplitude); // E field along x-axis
         DrawWave(magneticField, Vector3.up, amplitude / 2); // B field along y-axis
     }
@@ -40,6 +66,14 @@ public class ElectromagneticWave : MonoBehaviour
 
         return lr;
     }
+
+    void UpdateAmplitude(float value)
+    {
+        amplitude = value; // Update the amplitude based on the slider value
+        Debug.Log("Amplitude updated: " + amplitude);
+    }
+
+
 
     void DrawWave(LineRenderer lr, Vector3 axis, float amp)
     {
---
+++ Assets/SliderScript.cs
@@ -0,0 +1,23 @@
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using UnityEngine.UI;
+
+
+// public class SliderScript : MonoBehaviour
+// {
+//     [SerializeField] private Slider amplitudeSlider;
+//     // Start is called before the first frame update
+//     void Start()
+//     {
+//        amplitudeSlider.onValueChanged.AddListener((v=>){
+
+//        }) 
+//     }
+
+//     // Update is called once per frame
+//     void Update()
+//     {
+        
+//     }
+// }
